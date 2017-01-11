294e28124c1ff2e8d3cd2aac77ea4459976a8eaa
Vincent BESANCON <besancon.vincent@gmail.com>
2017-01-11 23:47:23 +0100

    Tested installer (#5). Do the job, to be continued.

--- installer.sh
+++ installer.sh
@@ -3,15 +3,32 @@
 # Easy to use installer script to setup latest stable release of Syncthing in a
 # Docker container.
 #
+# Run it with:
+#
+#   curl -sSL https://raw.githubusercontent.com/nVentiveUX/docker-syncthing/master/installer.sh | sudo bash
+#
 set -euo pipefail
 trap "echo 'ERROR: Script failed: see failed command above.'" ERR
 
 SYNCTHING_IMAGE='nventiveux/docker-syncthing'
-SYNCTHING_RELEASE='0.14.18'
+SYNCTHING_RELEASE='latest'
+
+GITHUB_ROOT='https://raw.githubusercontent.com/nVentiveUX/docker-syncthing/master'
+
+# Used for debugging with:
+#   python3 -m http.server 8080
+#GITHUB_ROOT='http://192.168.1.1:8080'
+
+# Download a file using curl from remote repository
+function repo_file_download() {
+    local src="$1"
+    local dest="$2"
+    curl -sSL "${GITHUB_ROOT}/${src}" -o "${dest}"
+}
 
 # Install Docker Engine
 function install_docker() {
-    if [[ -x docker ]]; then
+    if [[ ! -x "$(which docker)" ]]; then
         echo "## Docker is not found. Installing..."
         ( curl -sSL https://get.docker.com/ | sh )
     fi
@@ -24,12 +41,23 @@ function pull_image() {
 
 function install_systemd_service() {
     echo "## Installing systemd service..."
-    cp -v systemd/docker-syncthing.service /etc/systemd/system/ &&
-    cp -v systemd/docker-syncthing@.default /etc/default/docker-syncthing &&
-    systemctl daemon-reload &&
+    repo_file_download \
+        "systemd/docker-syncthing@.service" \
+        "/etc/systemd/system/docker-syncthing@.service"
+    repo_file_download \
+        "systemd/docker-syncthing.service" \
+        "/etc/systemd/system/docker-syncthing.service"
+
+    if [[ ! -e "/etc/default/docker-syncthing" ]]; then
+        repo_file_download \
+            "systemd/docker-syncthing@.default" \
+            "/etc/default/docker-syncthing"
+    fi
+
+    systemctl daemon-reload
 
     echo "### Enabling docker-syncthing at system boot..."
-    systemctl enable docker-syncthing.service;
+    systemctl enable docker-syncthing.service
 }
 
 function start_syncthing_service() {
@@ -40,10 +68,9 @@ function start_syncthing_service() {
 function configure() {
     echo
     echo "IMPORTANT NOTICE:"
-    echo "   You may now want to provide content to syncthing, please edit /etc/default/docker-syncthing to add volumes."
+    echo "   You may now want to provide content to syncthing, please edit /etc/default/docker-syncthing to configure volumes."
     echo "   Then, restart the service with \"sudo systemctl restart docker-syncthing\" to apply changes."
     echo
-    /etc/default/docker-syncthing
 }
 
 function show_success() {
@@ -56,8 +83,8 @@ function show_success() {
 install_docker
 pull_image
 install_systemd_service
-start_syncthing_service
 configure
+start_syncthing_service
 show_success
 
 exit 0
