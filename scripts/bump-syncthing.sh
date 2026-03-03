70bd02e40140cc6769ccbb3b1720b0821ec78dab
BESANCON Vincent <vb@vinzworld.fr>
2026-03-03 22:13:32 +0100

    feat(scripts): fetch latest release from github api for syncthing

--- scripts/bump-syncthing.sh
+++ scripts/bump-syncthing.sh
@@ -2,14 +2,16 @@
 
 set -eu
 
-next_version="${1:?Usage: $0 <next_version>}"
-
 # Checks
 if [[ -n $(git status --porcelain) ]]
 then
   echo "Repo is dirty. Commit all changes first !"
   exit 1
 fi
+
+# get latest stable version for syncthing
+next_version="$(curl -fsSL "https://api.github.com/repos/syncthing/syncthing/releases/latest" | jq -r '.tag_name | ltrimstr("v")')"
+echo "Fetched latest stable version: ${next_version}"
 
 # Bump all versions
 sed -i -r -e "s/SYNCTHING_VERSION=\".*\"/SYNCTHING_VERSION=\"${next_version}\"/" Dockerfile
